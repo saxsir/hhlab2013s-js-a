03df49d1e8830024952b68fe724ffd735fc2bcec
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-05-05 18:01:52 +0900

    デンドログラムは後回し, とりあえず途中まで書いたけどコメントアウトした
    先にk-meansをやる
    index.htmlをちょいと編集
    実行ボタンのidで処理を振り分けるようにした(app.js)

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,9 +1,9 @@
 $(function () {
   var clusters = new Clusters();
-  var blognames, words, data;
+  var blognames = [], words = [], data = [];
 
-  var $readBtn = $('#read-btn');
-  var $runHCluster = $('#run-hcluster');
+  var $readBtn = $('.read-btn');
+  var $runBtn = $('.run-btn');
 
   // ファイルを選択した時に実行される関数
   $readBtn.change(function (e) {
@@ -13,16 +13,30 @@ $(function () {
       words = result[1];
       data = result[2];
       console.log('Finish reading file.');
-      // クラスタリング実行ボタンを有効化する
-      $runHCluster.removeAttr("disabled");
     });
   });
 
-  // 実行ボタンが押されたら実行される関数
-  $runHCluster.click(function () {
-    // 二度押ししないように無効にしとく
-    $runHCluster.attr("disabled", "disabled");
-    var clust = clusters.hcluster(data);
-    console.dir(clust);
+  /*
+   * 実行ボタンが押されたら実行される関数
+   * ファイルが読み込まれていない場合は何もしない
+   */ 
+  $runBtn.click(function (e) {
+    var id = e.target.id;
+    if($readBtn.val() === '') {
+      console.log('ファイルを読み込んでから実行して下さい');
+      return;
+    }
+    switch(id) {
+      case 'run-hcluster':
+        console.log('Run hcluster');
+        var clust = clusters.hcluster(data);
+        console.log(clust);
+        break;
+      case 'run-draw2d':
+        console.log("Run draw2d");
+        break;
+      default:
+        console.log('Unknown case');
+    }
   });
 });
