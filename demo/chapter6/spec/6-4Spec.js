2ce503ae9e5861612b20ba97bed5f89b4b58b314
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-06-06 15:06:47 +0900

    Change demo script

--- demo/chapter6/spec/6-4Spec.js
+++ demo/chapter6/spec/6-4Spec.js
@@ -7,7 +7,7 @@ docclass.sampletrain(cl);
 //docclass.sampletrain(cl);
 //console.log("cl.weightedprob('money', 'good', cl.fprob): " + cl.weightedprob('money', 'good', cl.fprob)); // => 0.16666666666666666
 
-var result = "cl.fprob('quick', 'good'): " + cl.fprob('quick', 'good') + "\n";
+var result = "cl.fprob('money', 'good'): " + cl.fprob('money', 'good') + "\n";
 result += '\n';
 result += "cl.weightedprob('money', 'good', cl.fprob): " + cl.weightedprob('money', 'good', cl.fprob) + '\n';
 docclass.sampletrain(cl);
