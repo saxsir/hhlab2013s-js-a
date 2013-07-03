4bda6785b1a78e442db2d1b772493eff3241b375
Shohei Sasamoto <shohei.sasamoto@gmail.com>
2013-07-03 17:25:46 +0900

    9-8の交差検定まで実装完了
    - svm.jsに自前の交差検定関数を実装
    - 返す値はlivsvmとは違い、正答数を直接返す

---
+++ demo/chapter9/js/svm.crossValidation.js
@@ -0,0 +1,85 @@
+svmjs.SVM.prototype.crossValidation = function(data, labels, opts, k) {
+  // サブセットクラス
+  var Subset = function() {
+    this._data = [];
+    this._labels = [];
+  };
+
+  // データセットをk個のサブセットに分割する
+  var subsets = [];
+  for (var i = 0; i < k; i++) {
+    subsets.push(new Subset());
+  }
+
+  // ここでランダムに、かつ均等に分けないといけない。
+  var n = data.length;
+  for (var i = 0; i < n; i++) {
+    var group = i % k;
+    var rnd = Math.floor(Math.random() * data.length);
+    var d = data.splice(rnd, 1);
+    var l = labels.splice(rnd, 1);
+    Array.prototype.push.apply(subsets[group]._data, d);
+    Array.prototype.push.apply(subsets[group]._labels, l);
+  }
+ 
+  /*
+  data.forEach(function(d, i){
+    var group = i % k;
+    subsets[group]._data.push(d);
+    subsets[group]._labels.push(labels[i]);
+  });
+  */
+
+  console.log(subsets);
+
+  // console.log(subsets); // debug
+  // 1つのサブセットをテスト用にして、残りでsvmをトレーニングする
+  // これをk回（各サブセット分）繰り返す
+  // k回分の検証結果の配列を返す
+  var self = this;
+  var hit = 0;
+  subsets.forEach(function(subset, i){
+    var test_data = subset._data;
+    var test_label = subset._labels;
+    var training_data = [];
+    var training_labels = [];
+
+    // $.extendで値渡しで配列のコピー
+    var subsets_copy = $.extend(true, [], subsets);
+    subsets_copy.splice(i, 1);
+
+    // console.log('group ' + i); // debug
+    // console.log(test_data); // debug
+    // console.log(test_label); // debug
+    // console.log('subset');
+    // console.log(subsets);
+    // console.log('copy spliced');
+    // console.log(subsets_copy);
+
+    subsets_copy.forEach(function(other){
+      // どっちも同じだけどpushの方が圧倒的に早い
+      // training_data = training_data.concat(other._data);
+      // training_labels = training_labels.concat(other._labels);
+      Array.prototype.push.apply(training_data, other._data);
+      Array.prototype.push.apply(training_labels, other._labels);
+    });
+    var sub_svm = new svmjs.SVM();
+    // sub_svm.train(training_data, training_labels, { kernel: 'rbf', rbfsigma: 0.5 });
+    sub_svm.train(training_data, training_labels, opts);
+    // console.log(sub_svm.predict([ [0.65625,1,0,0.78125,0,1,0.3] ])); // -1
+    // console.log(sub_svm.predict([ [0.15625,0,0,0.375,0,0,0.2] ])); // 1
+
+    test_data.forEach(function(d, i){
+      //console.log(d);
+      var guess = sub_svm.predict([d]);
+      console.log('guess: ' + guess);
+      console.log('answer: ' + training_labels[i]);
+      if (guess == training_labels[i]) {
+        console.log('hit!');
+        hit++;
+      }
+    });
+  });
+  // return (num of all data)- (num of correct answers)
+  return hit;
+};
--- demo/chapter9/spec/9-8Spec.js
+++ demo/chapter9/spec/9-8Spec.js
@@ -1,17 +1,3 @@
-// とりあえずsvm.jsを使ってみる
-/*
-var data = [[0,0], [0,1], [1,0], [1,1]];
-var labels = [-1, 1, 1, -1];
-var svm = new svmjs.SVM();
-svm.train(data, labels, {C: 1.0}); // C is a parameter to SVM
-
-var testdata = [[0,1]]; // ??
-var testlabels = svm.predict(testdata); // predict → 予想する
-console.log(testlabels);
-*/
-
-
-
 var numericalset = advancedclassify.loadnumerical();
 var res = advancedclassify.scaledata(numericalset);
 var scaledset = res[0];
@@ -30,14 +16,25 @@ scaledset.forEach(function(r){
 answers.forEach(function(v, i, array){
   if (v == 0) {
     array[i] = '-1';
+  } else if (v == 1) {
+    array[i] = '1';
   }
 });
-console.log(answers);
 
 var svm = new svmjs.SVM();
 svm.train(inputs, answers, { kernel: 'rbf', rbfsigma: 0.5 });
+var newrow = [28.0, -1, -1, 26.0, -1, 1, 2, 0.8]; // 男は子どもを望んでいないが、女は望んでいる
+console.log(svm.predict([scalef(newrow)])); // 0.0
+
+newrow = [28.0, -1, 1, 26.0, -1, 1, 2, 0.8];  // 両者ともに子どもを望んでいる
+console.log(svm.predict([scalef(newrow)])); // 1.0
+
+var json = svm.toJSON();
+console.log(JSON.stringify(json));
 */
 
+// 結果見たいだけならここのコメントを外す
+/*
 var json = $('#svm_json').val();
 var svm = new svmjs.SVM();
 svm.fromJSON(JSON.parse(json));
@@ -47,3 +44,41 @@ console.log(svm.predict([scalef(newrow)])); // 0.0
 
 newrow = [28.0, -1, 1, 26.0, -1, 1, 2, 0.8];  // 両者ともに子どもを望んでいる
 console.log(svm.predict([scalef(newrow)])); // 1.0
+
+//  交差検定の確認
+console.log(svm.predict([[0.65625,1,0,0.78125,0,1,0.3]])); // -1
+console.log(svm.predict([ [0.1875, 0, 0, 0.71875, 0, 1, 0.2] ])); // 1
+*/
+
+// 交差検定
+var inputs = []; // data
+var answers = []; // labels
+
+scaledset.forEach(function(r){
+  inputs.push(r.data);
+  answers.push(r.match);
+});
+
+// データの変換, クラス0を-1にする（svm.jsが-1 or 1でクラス判別するので）
+answers.forEach(function(v, i, array){
+  if (v == 0) {
+    array[i] = '-1';
+  } else if (v == 1) {
+    array[i] = '1';
+  }
+});
+
+$('#crossValidation').click(function() {
+  var svm = new svmjs.SVM();
+  var num_of_correct_answers = svm.crossValidation(inputs, answers, { kernel: 'rbf', rbfsigma: 0.5 }, 2); // 正答数を返す
+  alert(num_of_correct_answers); // 
+});
+
+
+/*
+var sum = 0;
+guesses.forEach(function(g, i){
+  sum += Math.abs(answers[i] - g);
+});
+console.log(sum);
+*/
